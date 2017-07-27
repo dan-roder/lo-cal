ce6b99b6e0187d3a80eb12d527f306e9d3e60ee8
Dan Roder <droder@repequity.com>
2017-07-27 15:01:45 -0400

    setting up appropriate routes to use main layout then route from there

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
   }
 ]
 
