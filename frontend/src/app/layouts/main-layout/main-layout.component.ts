96f1a8799ef0e2b7662a2ccb4fd754359ca9887d
Dan Roder <droder@repequity.com>
2017-07-27 18:50:47 -0400

    pulling and displaying menu items

--- frontend/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { WordpressService } from '../../services/wp.service';
 
 @Component({
   selector: 'lo-cal-main-layout',
   templateUrl: './main-layout.component.html',
 })
 export class MainLayoutComponent implements OnInit {
+  private mainMenuLinks;
 
-  constructor() { }
+  constructor( private wpService: WordpressService ) { }
 
   ngOnInit() {
+    this.wpService.getMenu(2).subscribe(m => {this.mainMenuLinks = m.items; console.log(m.items)});
   }
 
 }
