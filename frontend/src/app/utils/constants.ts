f5e36ced619751543873b543e66a22e0140aef05
Dan Roder <droder@repequity.com>
2017-07-28 11:23:55 -0400

    commenting the constants file

--- frontend/src/app/utils/constants.ts
+++ frontend/src/app/utils/constants.ts
@@ -3,14 +3,16 @@ import { WindowRef } from './windowref';
 
 @Injectable()
 export class Config {
-  //public wordpressApiUrl = "http://dev-lo-cal.pantheonsite.io/wp-json"
-  public wordpressApiUrl:string = '';
+  public wordpressApiUrl : string = '';
 
   constructor(private winRef: WindowRef){
-    console.log(winRef.nativeWindow.location);
+    // Setting API URL based on current environment
     switch(winRef.nativeWindow.location.hostname){
       case 'localhost':
         this.wordpressApiUrl = "http://lo-cal.dev/wp-json";
+      break;
+      case 'live-lo-cal.pantheonsite.io':
+        this.wordpressApiUrl = "https://www.lo-calkitchen.com/wp-json";
       break;
       default:
         this.wordpressApiUrl = "http://" + winRef.nativeWindow.location.hostname + "/wp-json";
