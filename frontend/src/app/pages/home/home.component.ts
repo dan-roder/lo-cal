739d9e2273caf9e14e11d983d2c313eb6ebd7938
Dan Roder <droder@repequity.com>
2017-07-28 16:13:13 -0400

    pulling initial homepage data

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { WordpressService } from '../../services/wp.service';
 
 @Component({
   selector: 'lo-cal-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  private pageContent : object = {};
 
-  constructor() { }
+  constructor(private wpService: WordpressService) { }
 
   ngOnInit() {
+    this.wpService.getPage(4).subscribe(page => {this.pageContent = page; console.log(page)});
+
   }
 
 }
