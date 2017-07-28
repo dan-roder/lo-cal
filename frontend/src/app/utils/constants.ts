ddfdd9f25c2f84128156e92cec3341ddaa3855c3
Dan Roder <droder@repequity.com>
2017-07-28 10:26:58 -0400

    starting conditional for API URL using window ref service

--- frontend/src/app/utils/constants.ts
+++ frontend/src/app/utils/constants.ts
@@ -1,6 +1,20 @@
 import { Injectable } from "@angular/core";
+import { WindowRef } from './windowref';
 
 @Injectable()
 export class Config {
-  public wordpressApiUrl = "http://dev-lo-cal.pantheonsite.io/wp-json"
+  //public wordpressApiUrl = "http://dev-lo-cal.pantheonsite.io/wp-json"
+  public wordpressApiUrl:string = '';
+
+  constructor(private winRef: WindowRef){
+    switch(winRef.nativeWindow.location.hostname){
+      case 'localhost':
+        this.wordpressApiUrl = "http://lo-cal.dev/wp-json";
+      break;
+      default:
+        this.wordpressApiUrl = "http://" + winRef.nativeWindow.location.hostname + "/wp-json";
+      break;
+    }
+
+  }
 }
