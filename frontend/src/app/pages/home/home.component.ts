8653b06365351742d342685481a23149bcac3444
Dan Roder <droder@repequity.com>
2017-08-02 17:36:27 -0400

    laying out homepage

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -7,12 +7,18 @@ import { WordpressService } from '../../services/wp.service';
 })
 export class HomeComponent implements OnInit {
   public pageContent : any;
+  public featuredImage : any;
 
   constructor(private wpService: WordpressService) { }
 
   ngOnInit() {
-    this.wpService.getPage(4).subscribe(page => {this.pageContent = page; console.log(page)});
+    this.wpService.getPage(4).subscribe(page => {
+      this.pageContent = page;
 
+      if(page.featured_media != 0){
+        this.wpService.getMedia(page.featured_media).subscribe(media => {this.featuredImage = media; console.log(media)});
+      }
+    });
   }
 
 }
