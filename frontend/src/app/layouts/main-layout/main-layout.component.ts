a32842ced13ea9d28f4592fc3a7131272bcc04b1
Dan Roder <droder@repequity.com>
2017-08-15 14:09:55 -0400

    conditional for page title if current page is homepage

--- frontend/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { WordpressService } from '../../services/wp.service';
 
 @Component({
@@ -7,8 +8,11 @@ import { WordpressService } from '../../services/wp.service';
 })
 export class MainLayoutComponent implements OnInit {
   public mainMenuLinks;
+  public currentUrl : string = '';
 
-  constructor( private wpService: WordpressService ) { }
+  constructor( private wpService: WordpressService, private _router: Router ) {
+    _router.events.subscribe( (url : any) => this.currentUrl = url);
+  }
 
   ngOnInit() {
     this.wpService.getMenu(2).subscribe(m => this.mainMenuLinks = m.items);
