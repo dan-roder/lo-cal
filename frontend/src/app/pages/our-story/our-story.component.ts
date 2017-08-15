96a564bc6504534d9279de55cb9645d91ab317eb
Dan Roder <droder@repequity.com>
2017-08-15 13:30:08 -0400

    pulling content for our-story page

--- frontend/src/app/pages/our-story/our-story.component.ts
+++ frontend/src/app/pages/our-story/our-story.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { WordpressService } from '../../services/wp.service';
 
 @Component({
   selector: 'lo-cal-our-story',
   templateUrl: './our-story.component.html'
 })
 export class OurStoryComponent implements OnInit {
+  public pageContent : any;
+  public acf : any;
+  public featuredImage : any;
 
-  constructor() { }
+  constructor(private wpService: WordpressService) { }
 
   ngOnInit() {
+    this.wpService.getPage(47).subscribe(page => {
+      this.pageContent = page;
+      this.acf = page.acf;
+      console.log(this.pageContent);
+
+      if(page.featured_media != 0){
+        this.wpService.getMedia(page.featured_media).subscribe(media => this.featuredImage = media);
+      }
+    });
   }
 
 }
