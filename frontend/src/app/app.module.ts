920cfc4adb2e0ccf6ed0dfde658c30a6da264f08
Dan Roder <droder@repequity.com>
2017-08-16 17:12:32 -0400

    split intro section into component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,11 +15,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { TitleBodyImagesComponent } from './shared/components/title-body-images/title-body-images.component';
+import { IntroSectionComponent } from './shared/components/intro-section/intro-section.component';
 
 // Services
 import { WordpressService } from './services/wp.service';
 import { WindowRef } from './utils/windowref';
 import { OurStoryComponent } from './pages/our-story/our-story.component';
+
 
 
 @NgModule({
@@ -30,7 +32,8 @@ import { OurStoryComponent } from './pages/our-story/our-story.component';
     FooterComponent,
     OurStoryComponent,
     HeaderComponent,
-    TitleBodyImagesComponent
+    TitleBodyImagesComponent,
+    IntroSectionComponent
   ],
   imports: [
     BrowserModule,
