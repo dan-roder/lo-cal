c448b561908beec939c7557394954268f12540cb
Dan Roder <droder@repequity.com>
2017-08-16 17:13:55 -0400

    added landing_page custom post type

--- wp-content/themes/local/functions/post_types.php
+++ wp-content/themes/local/functions/post_types.php
@@ -1,42 +1,64 @@
 <?php
-  add_action('init', 'custom_post_types');
+  // Register Custom Post Type
+function custom_post_type() {
 
-  function custom_post_types(){
-    // register_post_type('leadership', array(
-    //   'label' => 'Leadership',
-    //   'description' => 'Kastle Security team members with bio and headshot.',
-    //   'public' => true,
-    //   'show_ui' => true,
-    //   'exclude_from_search' => false,
-    //   'show_in_menu' => true,
-    //   '_builtin' => false,
-    //   'capability_type' => 'post',
-    //   'hierarchical' => false,
-    //   'query_var' => true,
-    //   'menu_icon'   => 'dashicons-businessman',
-    //   'hierarchical' => false,
-    //   'has_archive' => true,
-    //   'supports' => array(
-    //   	'title',
-    //   	'page-attributes',
-    //   	'editor',
-    //   	'thumbnail'
-    //   ),
-    //   'labels' => array (
-    //     'name' => 'Leadership',
-    //     'singular_name' => 'Member',
-    //     'menu_name' => 'Team',
-    //     'add_new' => 'Add Team Member',
-    //     'add_new_item' => 'Add Team Member',
-    //     'edit' => 'Edit',
-    //     'edit_item' => 'Edit Member',
-    //     'new_item' => 'New Member',
-    //     'view' => 'View Team Member',
-    //     'view_item' => 'View Team Member',
-    //     'search_items' => 'Search Team Members',
-    //     'not_found' => 'No Team Members Found',
-    //     'not_found_in_trash' => 'No Team Members Found in Trash',
-    //   ),
-    // ));
+    $labels = array(
+      'name'                  => _x( 'Landing Pages', 'Post Type General Name', 'text_domain' ),
+      'singular_name'         => _x( 'Landing Page', 'Post Type Singular Name', 'text_domain' ),
+      'menu_name'             => __( 'Landing Pages', 'text_domain' ),
+      'name_admin_bar'        => __( 'Landing Page', 'text_domain' ),
+      'archives'              => __( 'Item Archives', 'text_domain' ),
+      'attributes'            => __( 'Item Attributes', 'text_domain' ),
+      'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+      'all_items'             => __( 'All Items', 'text_domain' ),
+      'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+      'add_new'               => __( 'Add New', 'text_domain' ),
+      'new_item'              => __( 'New Item', 'text_domain' ),
+      'edit_item'             => __( 'Edit Item', 'text_domain' ),
+      'update_item'           => __( 'Update Item', 'text_domain' ),
+      'view_item'             => __( 'View Item', 'text_domain' ),
+      'view_items'            => __( 'View Items', 'text_domain' ),
+      'search_items'          => __( 'Search Item', 'text_domain' ),
+      'not_found'             => __( 'Not found', 'text_domain' ),
+      'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+      'featured_image'        => __( 'Featured Image', 'text_domain' ),
+      'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+      'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+      'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+      'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+      'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+      'items_list'            => __( 'Items list', 'text_domain' ),
+      'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+      'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+    );
+    $args = array(
+      'label'                 => __( 'Landing Page', 'text_domain' ),
+      'description'           => __( 'Landing Pages with multiple different options for content to be displayed on the page', 'text_domain' ),
+      'labels'                => $labels,
+      'supports'              => array(
+        'title',
+        'editor',
+        'thumbnail',
+        'page-attributes'
+      ),
+      'show_in_rest'          => true,
+      'rest_base'             => 'landing_page',
+      'hierarchical'          => true,
+      'public'                => true,
+      'show_ui'               => true,
+      'show_in_menu'          => true,
+      'menu_position'         => 20,
+      'menu_icon'             => 'dashicons-images-alt',
+      'show_in_admin_bar'     => true,
+      'show_in_nav_menus'     => true,
+      'can_export'            => true,
+      'has_archive'           => true,
+      'exclude_from_search'   => false,
+      'publicly_queryable'    => true,
+      'capability_type'       => 'page',
+    );
+    register_post_type( 'landing_page', $args );
+
   }
+  add_action( 'init', 'custom_post_type', 0 );
 ?>
