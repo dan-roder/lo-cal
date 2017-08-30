1f0a3e9f9462871bb3babd80578f9ac90e53b2d9
Dan Roder <droder@repequity.com>
2017-08-30 16:58:34 -0400

    reusable component image and text

--- frontend/src/app/shared/components/image-and-text/image-and-text.component.ts
+++ frontend/src/app/shared/components/image-and-text/image-and-text.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lo-cal-image-and-text',
   templateUrl: './image-and-text.component.html'
 })
 export class ImageAndTextComponent implements OnInit {
+  @Input() acf : any;
+  @Input() color : string = 'white';
 
   constructor() { }
 
