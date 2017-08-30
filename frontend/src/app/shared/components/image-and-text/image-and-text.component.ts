03944b7acd4906b5380145ff884a90a8c1115b97
Dan Roder <droder@repequity.com>
2017-08-30 16:45:25 -0400

    new image and text component

---
+++ frontend/src/app/shared/components/image-and-text/image-and-text.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lo-cal-image-and-text',
+  templateUrl: './image-and-text.component.html'
+})
+export class ImageAndTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
