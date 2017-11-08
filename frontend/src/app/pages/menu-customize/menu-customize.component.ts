9a9cd82b17dd1f5db520042362a5d28947b7c688
Dan Roder <droder@repequity.com>
2017-11-08 11:38:00 -0500

    starting functions for menu item customize page

--- frontend/src/app/pages/menu-customize/menu-customize.component.ts
+++ frontend/src/app/pages/menu-customize/menu-customize.component.ts
@@ -5,10 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './menu-customize.component.html'
 })
 export class MenuCustomizeComponent implements OnInit {
+  private lettuceChoices: number = 0;
+  private maxLettuceChoices: number = 2;
+  private quantity: number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  toggleChoice(el){
+    console.log(el);
+  }
+
+  incrementQuantity(){
+    this.quantity++;
+  }
+
+  decrementQuantity(){
+    if(this.quantity > 0){
+      this.quantity--;
+    }
+  }
 }
