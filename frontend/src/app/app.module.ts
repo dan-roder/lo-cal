067d091a84968d634662fbd3dadcbedf79d324e2
Dan Roder <droder@repequity.com>
2017-11-13 16:29:12 -0500

    importing browser animations module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -58,7 +59,8 @@ import { WindowScrollDirective } from './shared/directives/window-scroll.directi
     FormsModule,
     HttpModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [ Config, WindowRef, WordpressService ],
   bootstrap: [AppComponent]
