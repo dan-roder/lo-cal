4db898c90d6714684ecd161e776ccb579b7d89df
Dan Roder <droder@repequity.com>
2018-02-26 16:05:44 -0500

    added local storage function to maybe be used later

--- frontend/src/app/services/menu-service.service.ts
+++ frontend/src/app/services/menu-service.service.ts
@@ -2,12 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Config } from '../utils/constants';
 import { Observable } from 'rxjs/Observable';
+import { Window } from 'selenium-webdriver';
+import { WindowRef } from '../utils/windowref';
 
 @Injectable()
 export class MenuService {
   public menuItemId: number = null;
+  public localMenu: any;
 
   constructor(private config: Config, private httpClient: HttpClient) { }
+
+  getFullMenuAndStore(){
+    // Using local storage
+    this.localMenu = this.get().subscribe(menu => {
+      localStorage.setItem('menu', JSON.stringify(menu));
+      return menu;
+    });
+  }
 
   getSubMenus(){
     return this.get('submenus');
@@ -17,9 +28,9 @@ export class MenuService {
     return this.get(subMenuItemName);
   }
 
-  get(apiMethod: string){
+  get(apiMethod: string = ''){
     let url = this.config.railsApi + '/' + apiMethod;
-
+    console.log(url);
     return this.httpClient.get(url);
   }
 
