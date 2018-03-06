eae7364d143fd66ef31a16ee6b867bfe28924bb4
Dan Roder <droder@repequity.com>
2018-03-06 17:25:26 -0500

    preparing catering form for submit functionality

--- frontend/src/app/pages/catering/catering.component.ts
+++ frontend/src/app/pages/catering/catering.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 import { WordpressService } from '@local/services/wp.service';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lo-cal-catering',
@@ -11,12 +12,25 @@ import { WordpressService } from '@local/services/wp.service';
 @AutoUnsubscribe()
 
 export class CateringComponent implements OnInit {
-
+  public cateringForm : FormGroup;
   public pageContent : any;
   public acf : any;
   public featuredImage : any;
+  public submittedOnce : boolean = false;
 
-  constructor(private wpService: WordpressService) { }
+  constructor(private wpService: WordpressService, public fb: FormBuilder) {
+    this.cateringForm = fb.group({
+      'full-name' : [null, Validators.required],
+      'phone' : [null, Validators.required],
+      'email' : [null, Validators.required, Validators.email],
+      'service-date' : [null, Validators.required],
+      'notes' : null
+    })
+  }
+
+  submitForm(formData){
+    this.submittedOnce = true;
+  }
 
   ngOnInit() {
     this.wpService.getCustomPostTypeById('landing_page', 126).subscribe(page => {
