0f9a5416662d9fc9bedbe5152e70504dbc04819c
Dan Roder <droder@repequity.com>
2018-03-16 12:53:24 -0400

    blog post component

---
+++ frontend/src/app/pages/blog-post/blog-post.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lo-cal-blog-post',
+  templateUrl: './blog-post.component.html'
+})
+export class BlogPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
