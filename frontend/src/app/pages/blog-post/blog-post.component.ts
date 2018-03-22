c9d82cc8b374db9ac4019207f5c1f12c19c328cf
Dan Roder <droder@repequity.com>
2018-03-22 17:47:35 -0400

    pulling blog post content

--- frontend/src/app/pages/blog-post/blog-post.component.ts
+++ frontend/src/app/pages/blog-post/blog-post.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { WordpressService } from '@local/services/wp.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
 @Component({
   selector: 'lo-cal-blog-post',
   templateUrl: './blog-post.component.html'
 })
-export class BlogPostComponent implements OnInit {
 
-  constructor() { }
+@AutoUnsubscribe()
+
+export class BlogPostComponent implements OnInit {
+  public pageContent : any;
+  public featuredImage : any;
+
+  constructor(private wordpressService: WordpressService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let slug = (this.route.snapshot.paramMap.get('post'));
+
+    this.wordpressService.getPostBySlug(slug, 'blog_post').subscribe(page => {
+      this.pageContent = page[0];
+
+      if(page[0].featured_media != 0){
+        this.wordpressService.getMedia(page[0].featured_media).subscribe(media => {this.featuredImage = media; console.log(media)});
+      }
+      console.log(page);
+    })
   }
 
+  ngOnDestroy(){}
 }
