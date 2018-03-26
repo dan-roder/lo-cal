d3a9c042e31f7a0a410cb9c9b31f702c69927ac3
Dan Roder <droder@repequity.com>
2018-03-26 11:23:09 -0400

    getting all blog posts

--- frontend/src/app/pages/blog/blog.component.ts
+++ frontend/src/app/pages/blog/blog.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { WordpressService } from '@local/services/wp.service';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
 @Component({
   selector: 'lo-cal-blog',
   templateUrl: './blog.component.html'
 })
-export class BlogComponent implements OnInit {
 
-  constructor() { }
+@AutoUnsubscribe()
+
+export class BlogComponent implements OnInit {
+  public featuredImage : any;
+  public allBlogPosts : any;
+
+  constructor(private wordpressService: WordpressService) { }
 
   ngOnInit() {
+    this.wordpressService.getCustomPostType('blog_post').subscribe(posts => {this.allBlogPosts = posts; console.log(posts);})
   }
 
 }
