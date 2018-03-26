0325d74b410bc3274222c1c72bfcd8225912d4f1
Dan Roder <droder@repequity.com>
2018-03-26 18:20:28 -0400

    pipe test file

---
+++ frontend/src/app/pipes/featured-image.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FeaturedImagePipe } from './featured-image.pipe';
+
+describe('FeaturedImagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FeaturedImagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
