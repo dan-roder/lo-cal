b5af3f356452d4301deafadd0d0b510f3edead0d
Dan Roder <droder@repequity.com>
2018-03-28 18:20:28 -0400

    SubMenu model created

---
+++ frontend/src/app/models/SubMenu.ts
@@ -0,0 +1,14 @@
+export interface SubMenu{
+  $id : string;
+  SubMenuId : number;
+  Name : string;
+  HeaderImageName : string;
+  Description : string;
+  Subtitle : any;
+  IsSelectable : boolean;
+  IsVisible : boolean;
+  Restrictions : any;
+  DisplayName : string;
+  MenuItems : any;
+  ExternalId : number;
+}
