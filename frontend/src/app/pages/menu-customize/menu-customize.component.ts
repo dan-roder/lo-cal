99a098f7dbdb96a5f8072168b3de37e7de34d31a
Dan Roder <droder@repequity.com>
2018-03-29 18:23:01 -0400

    pulled live data for menu item. functionality for selecting more than 1 of the item

--- frontend/src/app/pages/menu-customize/menu-customize.component.ts
+++ frontend/src/app/pages/menu-customize/menu-customize.component.ts
@@ -1,31 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { WordpressService } from '@local/services/wp.service';
+import { MenuService } from '@local/services/menu-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { IPost } from '@local/models/post';
 
 @Component({
   selector: 'lo-cal-menu-customize',
   templateUrl: './menu-customize.component.html'
 })
 export class MenuCustomizeComponent implements OnInit {
-  private lettuceChoices: number = 0;
-  private maxLettuceChoices: number = 2;
-  public quantity: number = 0;
+  private quantity: number = 1;
+  public itemContent : IPost;
+  public menuItemDetails : any;
+  public salesItemDetails : any;
+  public itemPrice : number;
+  public totalPrice : number;
 
-  constructor() { }
+  constructor(
+    private wpService: WordpressService,
+    private menuService: MenuService,
+    private router: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-  }
+    let slug = (this.router.snapshot.paramMap.get('item'));
 
+    this.wpService.getPostBySlug(slug, 'menu_item').subscribe(_item => {
+      let menuItemId = _item[0].acf.menuid;
+
+      this.getMenuItemDetails(menuItemId);
+    });
+  }
 
   toggleChoice(el){
     console.log(el);
   }
 
-  incrementQuantity(){
+  private incrementQuantity(){
     this.quantity++;
+    this.recalculateCost();
   }
 
-  decrementQuantity(){
-    if(this.quantity > 0){
+  private decrementQuantity(){
+    if(this.quantity > 1){
       this.quantity--;
+      this.recalculateCost();
     }
+  }
+
+  private recalculateCost(){
+    this.totalPrice = this.itemPrice * this.quantity;
+  }
+
+  getMenuItemDetails( _menuItemId ){
+    this.menuService.getMenuItemDetails(_menuItemId).subscribe(_menuItemDetails => {
+      // Set necessary variables for template rendering
+      this.menuItemDetails = _menuItemDetails;
+      this.salesItemDetails = _menuItemDetails['salesItems'][0];
+      this.itemPrice = _menuItemDetails['salesItems'][0]['Price'];
+      // Calculate initial cost based on initial quantity of 1
+      this.recalculateCost();
+    });
   }
 }
