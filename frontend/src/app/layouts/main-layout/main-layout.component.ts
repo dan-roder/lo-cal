4a9110b2cfe573ec857b32117866a9c21e6129a4
Dan Roder <droder@repequity.com>
2018-04-10 18:17:24 -0400

    set up main layout to retrieve data regarding bag/cart status

--- frontend/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { WordpressService } from '../../services/wp.service';
 import { WindowScrollDirective } from '../../shared/directives/window-scroll.directive';
+import { BagService } from '@local/services/bag.service';
+import { LineItem } from '@local/models/LineItem';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+
 
 @Component({
   selector: 'lo-cal-main-layout',
@@ -12,14 +16,25 @@ export class MainLayoutComponent implements OnInit {
   public currentUrl : any;
   public openBag: EventEmitter<any> = new EventEmitter<any>();
   public bagState: string = 'invisible';
+  public numberOfItemsInBag : number = 0;
+  public bagItems : Array<LineItem> = [];
 
 
-  constructor( private wpService: WordpressService, private _router: Router ) {
-    _router.events.subscribe( (url : any) => this.currentUrl = url);
-  }
+  constructor(
+    private wpService: WordpressService,
+    private bagService: BagService,
+    private localStorage: LocalStorage
+  ) { }
 
   ngOnInit() {
     this.wpService.getMenu(2).subscribe(m => this.mainMenuLinks = m.items);
+    // See if localStorage object for cart is populated
+    this.localStorage.getItem('bag').subscribe((bagItems) => {
+      this.bagItems = bagItems;
+      console.log(bagItems);
+    }, (error) => {
+      console.log(error);
+    })
   }
 
   toggleBagState(state){
