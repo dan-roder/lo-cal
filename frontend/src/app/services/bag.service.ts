7fa2dff49ea499ee00962620274a34cc96ae4825
Dan Roder <droder@repequity.com>
2018-04-11 17:31:07 -0400

    having bag service pull data from local storage. added getters and setters

--- frontend/src/app/services/bag.service.ts
+++ frontend/src/app/services/bag.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { LineItem } from '@local/models/LineItem';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class BagService {
-  public _menuItemsInBag : Array<any> = [];
   public _lineItems : Array<LineItem> = [];
+  public _itemsInBag : Array<LineItem> = [];
+  public _itemCountInBag : number = 0;
 
-
-  constructor() { }
+  constructor(protected localStorage: LocalStorage) {
+    this.localStorage.getItem('bag').subscribe(bagItemsFromLocalStorage => {
+      console.log('bag items retrieved', bagItemsFromLocalStorage);
+      this.itemsInBag = bagItemsFromLocalStorage;
+    })
+  }
 
   public createLineItem ( passedMenuItem ){
     let lineItem : LineItem = {};
@@ -22,23 +29,45 @@ export class BagService {
     lineItem.ExtendedPrice = lineItem.UnitPrice * lineItem.Quantity;
 
     // Push menuItem and lineItem into arrays
-    this._lineItems.push(lineItem);
+    this._itemsInBag.push(lineItem);
+
+    // Save to localStorage
+    this.saveToLocalStorage();
 
     lineItem = null;
-    console.log(this._lineItems);
   }
 
   public removeFromBagAtIndex( index ){
     // Remove item from both arrays
-    this._lineItems.splice(index, 1);
-    this._menuItemsInBag.splice(index, 1);
+    this._itemsInBag.splice(index, 1);
+    // Bag was modified, overwrite localStorage object with saved object
   }
 
-  // get menuItemsInBag() : any{
-  //   return this._menuItemsInBag;
-  // }
-
-  get lineItems() : Array<LineItem>{
-    return this._lineItems;
+  get itemCountInBag() : number{
+    return this._itemsInBag.length;
   }
+
+  get itemsInBag() : Array<LineItem>{
+    console.log('service bag items getter', this._itemsInBag);
+    return this._itemsInBag;
+  }
+
+  set itemsInBag(items: Array<LineItem>){
+    console.log('service bag items setter', items);
+    this._itemsInBag = items;
+  }
+
+  protected saveToLocalStorage(){
+    this.localStorage.setItem('bag', this.itemsInBag).subscribe(() => {
+      console.log('saved to localStorage', this.itemsInBag);
+    })
+  }
+
+  public retrieveBagFromStorage(): Observable<Array<LineItem>>{
+    return this.localStorage.getItem('bag').map(data => {
+      // this._itemsInBag = data;
+      return data;
+    })
+  }
+
 }
