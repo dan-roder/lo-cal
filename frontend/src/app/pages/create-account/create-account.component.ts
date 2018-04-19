c621c22909612c7b2f65eaf0f902bb2637d54296
Dan Roder <droder@repequity.com>
2018-04-19 14:25:21 -0400

    sending valid customer to rails api endpoint

--- frontend/src/app/pages/create-account/create-account.component.ts
+++ frontend/src/app/pages/create-account/create-account.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Customer, CustomerAddress, InRegistration } from '@local/models/Customer';
+import { RailsCustomer } from '@local/models/Customer';
 import { PasswordMatch } from '@local/utils/passwordmatch';
+import { CustomerService } from '@local/services/customer.service';
 
 
 @Component({
@@ -12,7 +13,7 @@ export class CreateAccountComponent implements OnInit {
   public accountForm : FormGroup;
   public submittedOnce : boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private customerService: CustomerService) {
     this.accountForm = fb.group({
       'first-name' : [null, Validators.required],
       'last-name' : [null, Validators.required],
@@ -36,40 +37,37 @@ export class CreateAccountComponent implements OnInit {
   ngOnInit() {
   }
 
-  showErrors(){
-    console.log(this.accountForm.controls['password']);
-  }
-
   public submitForm(form){
     this.submittedOnce = true;
-    console.log(form);
 
     // Set up customer object for submission to API
     if(form.valid) {
-      let registration : InRegistration = {
-        Customer : {
-          EMail : form.controls.email.value,
-          FirstName : form.controls['first-name'].value,
-          LastName : form.controls['last-name'].value,
-          Addresses : [{
-            AddressLine1 : form.controls['full-address'].controls.address.value,
-            AddressLine2 : form.controls['full-address'].controls.address2.value,
-            City : form.controls['full-address'].controls.city.value,
-            State : form.controls['full-address'].controls.state.value,
-            Postal : form.controls['full-address'].controls.zip.value
-          }]
-        },
-        Password : form.controls['password'].value,
-        SecurityQuestion : form.controls['security-question'].value,
-        SecurityAnswer : form.controls['security-answer'].value
+      let registration : RailsCustomer = {
+        customer_info : {
+          Customer : {
+            EMail : form.controls.email.value,
+            FirstName : form.controls['first-name'].value,
+            LastName : form.controls['last-name'].value,
+            Addresses : [{
+              AddressLine1 : form.controls['full-address'].controls.address.value,
+              AddressLine2 : form.controls['full-address'].controls.address2.value,
+              City : form.controls['full-address'].controls.city.value,
+              State : form.controls['full-address'].controls.state.value,
+              Postal : form.controls['full-address'].controls.zip.value
+            }]
+          },
+          Password : form.controls['password'].value,
+          SecurityQuestion : form.controls['security-question'].value,
+          SecurityAnswer : form.controls['security-answer'].value
+        }
       }
 
-      let customerDataForApi = {
-        "customer_info" : registration
-      }
-
-      // Call service to insert
-      console.log(customerDataForApi);
+      // Call service to create customer
+      this.customerService.createCustomer(registration);
     }
+  }
+
+  protected trimString(str : string){
+    return str.trim();
   }
 }
