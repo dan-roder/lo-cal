83ba4a93bfd0aa40ba085f6d269a5db4c32b370c
Dan Roder <droder@repequity.com>
2018-04-19 15:57:31 -0400

    changed login component to retrieve a login form component

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+
 @Component({
   selector: 'lo-cal-login',
   templateUrl: './login.component.html',
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
 
   ngOnInit() {
   }
