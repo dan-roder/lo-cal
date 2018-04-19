5ed264d852ea4133ea68228afba40fa2db2b95fb
Dan Roder <droder@repequity.com>
2018-04-19 16:53:58 -0400

    moved entire login form and functionality to new component

---
+++ frontend/src/app/shared/components/login-form/login-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from '@local/services/customer.service';
+import { RailsLogin } from '@local/models/Customer';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'lo-cal-login-form',
+  templateUrl: './login-form.component.html'
+})
+export class LoginFormComponent implements OnInit {
+  public loginForm : FormGroup;
+  public submittedOnce : boolean = false;
+  public errorData : any = {};
+
+  constructor(private fb: FormBuilder, private customerService: CustomerService, private localStorage: LocalStorage, private router: Router, private route: ActivatedRoute) {
+    this.loginForm = fb.group({
+      'email' : ['', [Validators.required, Validators.email]],
+      'password' : ['', Validators.required]
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  public login(loginFormData){
+    this.submittedOnce = true;
+
+    if(loginFormData.valid){
+      let loginDetails : RailsLogin = {
+        credentials : {
+          Email : loginFormData.controls['email'].value,
+          Password : loginFormData.controls['password'].value
+        }
+      }
+
+      this.customerService.logIn(loginDetails).subscribe(customerId => {
+        this.localStorage.setItem('user', customerId).subscribe(() => {
+          console.log(this.router);
+          console.log(this.route);
+        });
+      }, error => {
+        this.errorData = error;
+      })
+    }
+  }
+}
