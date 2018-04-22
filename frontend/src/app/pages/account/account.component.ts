c280d2a78e3caeb804e208e7a933ae4db05cefe4
Dan Roder <droder@repequity.com>
2018-04-21 20:04:16 -0400

    stubbed out functions for editing and saving account details

--- frontend/src/app/pages/account/account.component.ts
+++ frontend/src/app/pages/account/account.component.ts
@@ -2,16 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '@local/services/customer.service';
 import { Customer } from '@local/models/Customer';
 import { LocalStorage } from '@ngx-pwa/local-storage';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { PasswordMatch } from '@local/utils/passwordmatch';
 
 @Component({
   selector: 'lo-cal-account',
   templateUrl: './account.component.html'
 })
 export class AccountComponent implements OnInit {
-  private customer : Customer;
+  public customer : Customer;
   private customerId : string = '';
+  public accountForm : FormGroup;
+  public editing : boolean = false;
 
-  constructor(private customerService: CustomerService, private localStorage : LocalStorage) { }
+  constructor(private customerService: CustomerService, private localStorage : LocalStorage, private fb: FormBuilder) {
+    this.accountForm = fb.group({
+      'first-name' : [null, Validators.required],
+      'last-name' : [null, Validators.required],
+      'email' : [null, [Validators.required, Validators.email]],
+      'password' : ['', [Validators.required, Validators.minLength(8)]],
+      'confirm-password' : ['', Validators.required],
+      'full-address' : fb.group({
+        'address' : [null, Validators.required],
+        'address2' : null,
+        'city' : [null, Validators.required],
+        'state' : ['', Validators.required],
+        'zip' : [null, [Validators.required, Validators.pattern('^[0-9]{5}$')]]
+      })
+    })
+  }
 
   ngOnInit() {
     // Get customer ID
@@ -22,6 +41,17 @@ export class AccountComponent implements OnInit {
         console.log(customerData);
       })
     })
+  }
+
+  editAccountDetails(){
+    this.editing = true;
+
+    return false;
+  }
+
+  saveAccountDetails(formData){
+    console.log(formData);
+    this.editing = false;
   }
 
 }
