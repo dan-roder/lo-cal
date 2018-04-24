b6391ff4e86990a2803498446ba048593fc41642
Dan Roder <droder@repequity.com>
2018-04-24 09:38:31 -0400

    guard now working properly for logged in sections

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -2,24 +2,27 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { LocalStorage } from '@ngx-pwa/local-storage';
+import { CustomerService } from '@local/services/customer.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   private currentUser : any;
 
-  constructor(private router: Router, private localStorage: LocalStorage){
+  constructor(private router: Router, private localStorage: LocalStorage, private customerService: CustomerService){
     this.localStorage.getItem('user').subscribe(user => {this.currentUser = user;});
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if(this.currentUser !== null){
-        // Already logged in
-        return true;
-      }
-
-    this.router.navigate(['login'], { queryParams: { returnUrl: state.url }});
-    return false;
+      return this.customerService.isLoggedIn().map(data => {
+        if(data === null || data === undefined){
+          this.router.navigate(['login'], { queryParams: { returnUrl: state.url }});
+          return false;
+        }
+        else{
+          return true;
+        }
+      });
   }
 }
