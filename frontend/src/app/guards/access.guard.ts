eabfd42a8597512e9f2b7d91f6edd5c80d8bfa18
Dan Roder <droder@repequity.com>
2018-04-24 14:15:03 -0400

    added redirect for checkout login

--- frontend/src/app/guards/access.guard.ts
+++ frontend/src/app/guards/access.guard.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { CustomerService } from '@local/services/customer.service';
 
 @Injectable()
 export class AccessGuard implements CanActivate {
+
+  constructor(private router: Router, private customerService: CustomerService){ }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+      return this.customerService.isLoggedIn().map(data => {
+        if(data === null || data === undefined){
+          this.router.navigate(['checkout'], { queryParams: { returnUrl: state.url }});
+          return false;
+        }
+        else{
+          return true;
+        }
+      });
   }
 }
