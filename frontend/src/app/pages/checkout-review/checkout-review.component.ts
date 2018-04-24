99143088a287c3010c0237c5543b6434b73f7a1e
Dan Roder <droder@repequity.com>
2018-04-24 14:15:30 -0400

    checkout review created

---
+++ frontend/src/app/pages/checkout-review/checkout-review.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lo-cal-checkout-review',
+  templateUrl: './checkout-review.component.html'
+})
+export class CheckoutReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
