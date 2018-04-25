c1d2594225539fe36f2abab88cb6df81bcfd37ef
Dan Roder <droder@repequity.com>
2018-04-25 19:26:41 -0400

    pulling bagItems into checkout review component from service

--- frontend/src/app/pages/checkout-review/checkout-review.component.ts
+++ frontend/src/app/pages/checkout-review/checkout-review.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { BagService } from '@local/services/bag.service';
+import { LineItem } from '@local/models/LineItem';
 
 @Component({
   selector: 'lo-cal-checkout-review',
   templateUrl: './checkout-review.component.html'
 })
 export class CheckoutReviewComponent implements OnInit {
+  public _bagItems : Array<LineItem> = [];
 
-  constructor() { }
+  constructor(private bagService: BagService) { }
 
   ngOnInit() {
   }
 
+  get bagItems(){
+    return this.bagService.itemsInBag;
+  }
 }
