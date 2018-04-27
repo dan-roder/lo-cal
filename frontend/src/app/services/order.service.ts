1f2201a18a673bf57b0009bfe2e9791f18e43f44
Dan Roder <droder@repequity.com>
2018-04-27 18:26:51 -0400

    created order service

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { Order } from '@local/models/Order';
+import { LineItem } from '@local/models/LineItem';
 
 @Injectable()
 export class OrderService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private localStorage: LocalStorage) { }
+
+  public putOrder(bagItems: Array<LineItem>){
+    console.log(bagItems);
+    let order : Order = {
+
+    }
+  }
 
 }
