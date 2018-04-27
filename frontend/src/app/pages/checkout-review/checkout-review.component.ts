757959449ddb8b9987e2e5915e2cd3728a481348
Dan Roder <droder@repequity.com>
2018-04-27 14:58:43 -0400

    toggle functions for editing items in cart. quantity functions

--- frontend/src/app/pages/checkout-review/checkout-review.component.ts
+++ frontend/src/app/pages/checkout-review/checkout-review.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BagService } from '@local/services/bag.service';
 import { LineItem } from '@local/models/LineItem';
+import { Config } from '@local/utils/constants';
 
 @Component({
   selector: 'lo-cal-checkout-review',
@@ -8,10 +9,13 @@ import { LineItem } from '@local/models/LineItem';
 })
 export class CheckoutReviewComponent implements OnInit {
   public _bagItems : Array<LineItem> = [];
+  public isOpen: number = -1;
+  public envUrl : string = '';
 
-  constructor(private bagService: BagService) { }
+  constructor(private bagService: BagService, private constants: Config) { }
 
   ngOnInit() {
+    // this.envUrl = this.constants.wordpressApiUrl
   }
 
   get bagItems(){
@@ -21,4 +25,26 @@ export class CheckoutReviewComponent implements OnInit {
   public removeFromBagAtIndex( index ){
     this.bagService.removeFromBagAtIndex(index);
   }
+
+  public editItemAtIndex(newVal: number): boolean {
+    if (this.isOpen === newVal) {
+      this.isOpen = -1;
+    } else {
+      this.isOpen = newVal;
+    }
+
+    return false;
+  }
+
+  public incrementQuantity(menuItem){
+    menuItem.Quantity++;
+  }
+
+  public decrementQuantity(menuItem){
+    if(menuItem.Quantity > 1){
+      menuItem.Quantity--;
+    }
+  }
+
+
 }
