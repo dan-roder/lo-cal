5ce6f6e9152930cdaa7d21558c46efe7adc798c2
Dan Roder <droder@repequity.com>
2018-04-30 19:41:17 -0400

    retrieving saved order and pulling full order details

--- frontend/src/app/pages/checkout-review/checkout-review.component.ts
+++ frontend/src/app/pages/checkout-review/checkout-review.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BagService } from '@local/services/bag.service';
 import { LineItem } from '@local/models/LineItem';
 import { Config } from '@local/utils/constants';
+import { OrderService } from '@local/services/order.service';
+import { OrderResults, Order } from '@local/models/Order';
 
 @Component({
   selector: 'lo-cal-checkout-review',
@@ -11,11 +13,24 @@ export class CheckoutReviewComponent implements OnInit {
   public _bagItems : Array<LineItem> = [];
   public isOpen: number = -1;
   public envUrl : string = '';
+  private currentOrder : any;
+  public allOrderDetails : Order;
 
-  constructor(private bagService: BagService, private constants: Config) { }
+  constructor(private bagService: BagService, private constants: Config, private orderService: OrderService) { }
 
   ngOnInit() {
     // this.envUrl = this.constants.wordpressApiUrl
+
+    this.orderService.getPreSavedOrder().subscribe(orderResult => {
+      this.currentOrder = orderResult;
+      console.log(orderResult);
+      let orderId = orderResult.OrderId;
+      // Retrieve full order details from NCR
+      this.orderService.getFullOrderDetails(orderId).subscribe(orderDetails => {
+        console.log(orderDetails);
+        this.allOrderDetails = orderDetails;
+      });
+    });
   }
 
   get bagItems(){
