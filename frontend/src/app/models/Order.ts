8cd1f55c88fe1f9cdcdac6b51c8d6fe71efda9de
Dan Roder <droder@repequity.com>
2018-05-07 12:45:11 -0400

    added all interface keys for payment

--- frontend/src/app/models/Order.ts
+++ frontend/src/app/models/Order.ts
@@ -82,9 +82,16 @@ export interface InSubmitOrderInformation{
 }
 
 export interface Payment{
-  PaymentMethod: string; // 0 - SecureCreditCardPayment (saved), 1 - ClearCreditCardPayment, 2 - PayAtSitePayment
+  PaymentMethod: number; // 0 - SecureCreditCardPayment (saved), 1 - ClearCreditCardPayment, 2 - PayAtSitePayment
   AccountId?: number; // Used for SecureCreditCardPayment
   AccountNumber?: number; // Credit card number
+  ExpirationDate?: string; // The expiration date for the credit card required for ClearCreditCardPayment
+  SecurityCode?: string; // The security code for the credit card required for ClearCreditCardPayment
+  Amount?: number; // Amount to be paid for this payment method. A positive non-zero amount is required when applying stored value payments.
+  ZipCode?: string; // The billing zip code for the credit card, used with the ClearCreditCardPayment enum
+  CardNumber?: string; // The billing zip code for the credit card, used with the ClearCreditCardPayment enum
+  Pin?: string; // Pin for Stored Value Card payment. Required if attempting to apply stored value and if company setting "Aloha Enterprise ePin Validation Required" is set to true.
+  PaymentMethodType?: number; // <i>Nullable</i> What kind of card is passed in, used with ClearCreditCardPayment enum NOTE: Include this field with every payment.
 }
 
 export interface Vehicle{
