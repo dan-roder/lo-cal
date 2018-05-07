611482ccd8fdc63a4341161872223b3f75d8811b
Dan Roder <droder@repequity.com>
2018-05-07 12:45:27 -0400

    added payment type map to constants for returning correct number of payment type

--- frontend/src/app/utils/constants.ts
+++ frontend/src/app/utils/constants.ts
@@ -11,6 +11,17 @@ export class Config {
   public railsTimeEndpoint : string = 'http://138.197.123.70/times/1';
   public siteId : number = 1;
   public menuId : number = 1001;
+  public paymentTypeMap = {
+    "American Express" : 0,
+    "Credit" : 1,
+    "Diners Club" : 2,
+    "Discover" : 3,
+    "GiftCard" : 4,
+    "JCB" : 5,
+    "Mastercard" : 6,
+    "Visa" : 7,
+    "PrePaid" : 8
+  }
 
   constructor(private winRef: WindowRef){
     // Setting API URL based on current environment
