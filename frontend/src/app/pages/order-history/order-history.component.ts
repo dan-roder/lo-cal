14a44063aa048259001663ebaaa9405bec48f664
Dan Roder <dsroder@gmail.com>
2018-05-30 15:39:50 -0400

    order history component

---
+++ frontend/src/app/pages/order-history/order-history.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lo-cal-order-history',
+  templateUrl: './order-history.component.html'
+})
+export class OrderHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
