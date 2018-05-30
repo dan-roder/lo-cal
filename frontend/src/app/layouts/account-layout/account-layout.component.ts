d5913384fa1b0186a3fc2c7faa8d4b30535f9d4e
Dan Roder <dsroder@gmail.com>
2018-05-30 18:31:58 -0400

    made account screens into a global layout

---
+++ frontend/src/app/layouts/account-layout/account-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lo-cal-account-layout',
+  templateUrl: './account-layout.component.html',
+  styleUrls: ['./account-layout.component.scss']
+})
+export class AccountLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
