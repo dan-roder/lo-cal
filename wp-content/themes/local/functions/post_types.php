50658c0d85c5f7c7e6a7495d613fc655af806f88
Ken Crocken <kcrocken@repequity.com>
2018-06-07 14:33:06 -0400

    update metadata immediately

--- wp-content/themes/local/functions/post_types.php
+++ wp-content/themes/local/functions/post_types.php
@@ -230,45 +230,45 @@ function menu_categories_callback($post) {
  */
 function save_menu_categories_meta_box_data( $post_id ) {
 
-    // Check if our nonce is set.
-    if ( ! isset( $_POST['menu_category_nonce'] ) ) {
-        return;
-    }
+    // // Check if our nonce is set.
+    // if ( ! isset( $_POST['menu_category_nonce'] ) ) {
+    //     return;
+    // }
 
-    // Verify that the nonce is valid.
-    if ( ! wp_verify_nonce( $_POST['menu_category_nonce'], 'menu_category_nonce' ) ) {
-        return;
-    }
+    // // Verify that the nonce is valid.
+    // if ( ! wp_verify_nonce( $_POST['menu_category_nonce'], 'menu_category_nonce' ) ) {
+    //     return;
+    // }
 
-    // If this is an autosave, our form has not been submitted, so we don't want to do anything.
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-        return;
-    }
+    // // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+    // if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+    //     return;
+    // }
 
-    // Check the user's permissions.
-    if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+    // // Check the user's permissions.
+    // if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
 
-        if ( ! current_user_can( 'edit_page', $post_id ) ) {
-            return;
-        }
+    //     if ( ! current_user_can( 'edit_page', $post_id ) ) {
+    //         return;
+    //     }
 
-    }
-    else {
+    // }
+    // else {
 
-        if ( ! current_user_can( 'edit_post', $post_id ) ) {
-            return;
-        }
-    }
+    //     if ( ! current_user_can( 'edit_post', $post_id ) ) {
+    //         return;
+    //     }
+    // }
 
-    /* OK, it's safe for us to save the data now. */
+    // /* OK, it's safe for us to save the data now. */
 
-    // Make sure that it is set.
-    if ( ! isset( $_POST['menu_category'] ) ) {
-        return;
-    }
+    // // Make sure that it is set.
+    // if ( ! isset( $_POST['menu_category'] ) ) {
+    //     return;
+    // }
 
-    // Sanitize user input.
-    $my_data = sanitize_text_field( $_POST['menu_category'] );
+    // // Sanitize user input.
+    // $my_data = sanitize_text_field( $_POST['menu_category'] );
 
     // Update the meta field in the database.
     update_post_meta( $post_id, 'menu_category', $my_data );
