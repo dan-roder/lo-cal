f24502541fe62391b6f1e1e3f9686105b877fa68
Ken Crocken <kcrocken@repequity.com>
2018-06-07 15:20:54 -0400

    update metadata immediately

--- wp-content/themes/local/functions/post_types.php
+++ wp-content/themes/local/functions/post_types.php
@@ -223,48 +223,47 @@ function menu_categories_meta_box() {
 function save_menu_categories_meta_box_data( $post_id ) {
 
     // // Check if our nonce is set.
-    // if ( ! isset( $_POST['menu_category_nonce'] ) ) {
-    //     return;
-    // }
-	// $value =! isset( $_POST['menu_category_nonce'] );
+    if ( ! isset( $_POST['menu_category_nonce'] ) ) {
+        return;
+    }
 
     // // Verify that the nonce is valid.
-    // if ( ! wp_verify_nonce( $_POST['menu_category_nonce'], 'menu_category_nonce' ) ) {
-    //     return;
-    // }
+    if ( ! wp_verify_nonce( $_POST['menu_category_nonce'], 'menu_category_nonce' ) ) {
+        return;
+    }
 
     // // If this is an autosave, our form has not been submitted, so we don't want to do anything.
-    // if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-    //     return;
-    // }
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
 
     // // Check the user's permissions.
-    // if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+    if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
 
-    //     if ( ! current_user_can( 'edit_page', $post_id ) ) {
-    //         return;
-    //     }
+        if ( ! current_user_can( 'edit_page', $post_id ) ) {
+            return;
+        }
 
-    // }
-    // else {
+    }
+    else {
 
-    //     if ( ! current_user_can( 'edit_post', $post_id ) ) {
-    //         return;
-    //     }
-    // }
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+    }
 
     // /* OK, it's safe for us to save the data now. */
 
     // // Make sure that it is set.
-    // if ( ! isset( $_POST['menu_category'] ) ) {
-    //     return;
-    // }
+    if ( ! isset( $_POST['parent_id'] ) ) {
+        return;
+    }
 
     // // Sanitize user input.
     $my_data = sanitize_text_field( $_POST['parent_id'] );
 
     // Update the meta field in the database.
-    update_post_meta( $post_id, 'parent_id', $my_data );
+    update_post_meta( $post_id, 'menu_category', $my_data );
 }
 
 add_action( 'save_post', 'save_menu_categories_meta_box_data' );
