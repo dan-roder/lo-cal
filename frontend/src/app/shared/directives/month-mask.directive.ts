71f9061c0012a907bfe530e89aeddb24e27dfd27
Dan Roder <dsroder@gmail.com>
2018-06-07 17:16:51 -0400

    set up month mask to insert / for expiration date

--- frontend/src/app/shared/directives/month-mask.directive.ts
+++ frontend/src/app/shared/directives/month-mask.directive.ts
@@ -14,12 +14,10 @@ export class MonthMaskDirective {
   constructor(public model: NgControl) { }
 
   onInputChange(e, backspace){
-    console.log(e);
-    // let x = e.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-    // let rawValue = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-    // console.log( rawValue );
-    // // new value
-    // this.model.valueAccessor.writeValue( rawValue );
-    // this.rawChange.emit( rawValue );
+    let x = e.replace(/\D/g, '').match(/(\d{0,2})(\d{0,4})/);
+    let rawValue = !x[2] ? x[1] : x[1] + '/' + x[2];
+    // new value
+    this.model.valueAccessor.writeValue( rawValue );
+    this.rawChange.emit( rawValue );
   }
 }
