4ae67f463b9def60e6bfc872a3f3b0327ef7553d
Ken Crocken <kcrocken@repequity.com>
2018-06-07 16:44:13 -0400

    update metadata immediately

--- wp-content/themes/local/functions/post_types.php
+++ wp-content/themes/local/functions/post_types.php
@@ -382,7 +382,6 @@ add_filter( 'post_type_link', function( $link, $post ) {
 add_action( 'rest_api_init', 'create_api_posts_meta_field' );
 
 function create_api_posts_meta_field() {
-
 	// register_rest_field ( 'name-of-post-type', 'name-of-field-to-return', array-of-callbacks-and-schema() )
 	register_rest_field( 'menu_item', 'menu_category', array(
 		'get_callback' => 'get_post_meta_for_api',
@@ -395,7 +394,7 @@ function get_post_meta_for_api( $object ) {
 	//get the id of the post object array
 	$post_id = $object['id'];
 	//return the post meta
-	return get_post_meta( $post_id );
+	return get_post_meta( $post_id )['menu_category'][0];
 }
 
 add_action( 'init', 'blog_post', 0 );
