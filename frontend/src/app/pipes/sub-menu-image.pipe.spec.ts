d40cfb8e6312336cebdefea8508f17be21288232
Dan Roder <dsroder@gmail.com>
2018-06-17 14:33:26 -0400

    deleted menu image pipe

--- frontend/src/app/pipes/sub-menu-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SubMenuImagePipe } from './sub-menu-image.pipe';
-
-describe('SubMenuImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SubMenuImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
