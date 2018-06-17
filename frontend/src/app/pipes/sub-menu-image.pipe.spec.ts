584b1f00fa19f6df311afd8a3d594a6094d988a9
Dan Roder <dsroder@gmail.com>
2018-06-17 12:51:27 -0400

    sub menu image pipe

---
+++ frontend/src/app/pipes/sub-menu-image.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SubMenuImagePipe } from './sub-menu-image.pipe';
+
+describe('SubMenuImagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SubMenuImagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
