cbe9ec42df1bb3f4e2c24fbcaefd618f363530eb
Dan Roder <dsroder@gmail.com>
2018-06-17 22:20:03 -0400

    order history

--- frontend/src/app/pages/order-history/order-history.component.ts
+++ frontend/src/app/pages/order-history/order-history.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '@local/services/customer.service';
+import { Order } from '@local/models/Order';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
 @Component({
   selector: 'lo-cal-order-history',
   templateUrl: './order-history.component.html'
 })
-export class OrderHistoryComponent implements OnInit {
 
-  constructor() { }
+@AutoUnsubscribe()
+
+export class OrderHistoryComponent implements OnInit {
+  public orderHistory: Array<Order>;
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
+    this.customerService.isLoggedIn().subscribe(customer => {
+      this.customerService.getOrderHistory(customer.CustomerId).subscribe(orderHistory => {
+        this.orderHistory = orderHistory;
+      })
+    });
+  }
+
+  ngOnDestroy(){
+
   }
 
 }
