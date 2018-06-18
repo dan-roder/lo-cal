9a450ad210a2d55fff3c04c3a0d957cc48062eb6
Dan Roder <dsroder@gmail.com>
2018-06-17 22:49:34 -0400

    added customerId to auth guard

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       return this.customerService.isLoggedIn().map(data => {
-        if(data === null || data === undefined){
+        if(data === null || data === undefined || data.CustomerId === undefined){
           this.router.navigate(['login'], { queryParams: { returnUrl: state.url }});
           return false;
         }
