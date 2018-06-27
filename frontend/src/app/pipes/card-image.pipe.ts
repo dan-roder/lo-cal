92f75b558ec7b3a2b47ff87636f4599dce73a5fe
Dan Roder <dsroder@gmail.com>
2018-06-27 18:09:42 -0400

    catching error cases with pulling image

--- frontend/src/app/pipes/card-image.pipe.ts
+++ frontend/src/app/pipes/card-image.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { WordpressService } from '@local/services/wp.service';
 import * as _ from 'lodash';
+import { Observable } from 'rxjs';
 
 @Pipe({
   name: 'cardImage'
@@ -11,23 +11,23 @@ export class CardImagePipe implements PipeTransform {
   constructor(private wpService: WordpressService) { }
 
   transform(alohaMenuId: any, menuItemMap?: any) {
-    console.log(alohaMenuId);
     if(alohaMenuId) {
       let postObj = _.find(menuItemMap, {'menuid' : String(alohaMenuId)});
-      console.log(postObj);
 
-      return this.wpService.getCustomPostTypeById('menu_item', postObj.id).map(post => {
-        console.log(post);
-        if(post.acf.submenu_image !== undefined){
-          return post.acf.submenu_image.url;
-        }
-        else{
-          return "//via.placeholder.com/265x115";
-        }
-      });
+      if(postObj !== undefined){
+        return this.wpService.getCustomPostTypeById('menu_item', postObj.id).map(post => {
+          if(post.acf.submenu_image !== undefined){
+            return post.acf.submenu_image.url;
+          }
+          else{
+            return "//via.placeholder.com/265x115";
+          }
+        });
+      }
+      else{
+        return Observable.of("//via.placeholder.com/265x115");
+      }
     }
-
-    return "//via.placeholder.com/265x115";
   }
 
 }
