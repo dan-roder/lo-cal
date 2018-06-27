ca267762b055c904fc2dfa0b4bc40496671121d1
Dan Roder <dsroder@gmail.com>
2018-06-27 17:01:37 -0400

    cardImage pipe

---
+++ frontend/src/app/pipes/card-image.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { WordpressService } from '@local/services/wp.service';
+import * as _ from 'lodash';
+
+@Pipe({
+  name: 'cardImage'
+})
+export class CardImagePipe implements PipeTransform {
+
+  constructor(private wpService: WordpressService) { }
+
+  transform(alohaMenuId: any, menuItemMap?: any) {
+    if(alohaMenuId) {
+      let postObj = _.find(menuItemMap, {'menuid' : String(alohaMenuId)});
+
+      return this.wpService.getCustomPostTypeById('menu_item', postObj.id).map(post => {
+        if(post.acf.submenu_image !== undefined){
+          return post.acf.submenu_image.url;
+        }
+        else{
+          return "//via.placeholder.com/265x115";
+        }
+      });
+    }
+
+    return "//via.placeholder.com/265x115";
+  }
+
+}
