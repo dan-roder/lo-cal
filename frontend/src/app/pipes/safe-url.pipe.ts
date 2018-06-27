88defdb1c953c82efc94296678227e33e757ec7e
Dan Roder <dsroder@gmail.com>
2018-06-27 10:01:04 -0400

    added global flag to url regex

--- frontend/src/app/pipes/safe-url.pipe.ts
+++ frontend/src/app/pipes/safe-url.pipe.ts
@@ -6,9 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SafeUrlPipe implements PipeTransform {
 
   transform(value: any): any {
-    value = value.toLowerCase();
-    let noChars = value.replace(/[^a-z -]/, '');
-    let doubleHyphens = noChars.replace(/[-]{2,}/, '-');
+    let lowercase = value.toLowerCase();
+    let noChars = lowercase.replace(/[^a-z -]/g, '');
+    let doubleHyphens = noChars.replace(/[-]{2,}/g, '-');
     let properUrl = doubleHyphens.replace(/\s+/g, '-');
 
     return properUrl;
