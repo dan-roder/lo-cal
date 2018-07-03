e4f54894306304bef4bf96228920ddce63224dec
Dan Roder <dsroder@gmail.com>
2018-07-02 21:30:59 -0400

    added todo for cardType hash map

--- frontend/src/app/pages/confirmation/confirmation.component.ts
+++ frontend/src/app/pages/confirmation/confirmation.component.ts
@@ -17,10 +17,12 @@ export class ConfirmationComponent implements OnInit {
   constructor(private localStorage: LocalStorage, private bagService: BagService, private orderService: OrderService) { }
 
   ngOnInit() {
-    // TODO: Destroy Bag and Order from LocalStorage
     this.localStorage.removeItem('order').subscribe(() => {});
     this.localStorage.removeItem('bag').subscribe(() => {});
     this.bagService.itemsInBag = [];
+
+    // TODO: API treats cardType differently in results. Need to create 2nd hash map to display more detailed payment info
+    //  https://api.alohaorderonline.swenglabs.ncr.com/Help/ResourceModel?modelName=PaymentCardType
 
     this.orderService.getOrderResult().subscribe(result => {this.orderResult = result; console.log(result)});
   }
