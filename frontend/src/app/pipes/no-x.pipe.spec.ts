20f85b7a0f291b7cb4d8cd4c2d1ba59d5e09a16f
Dan Roder <dsroder@gmail.com>
2018-07-03 17:07:47 -0400

    pipe to get rid of * and x

---
+++ frontend/src/app/pipes/no-x.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NoXPipe } from './no-x.pipe';
+
+describe('NoXPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NoXPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
