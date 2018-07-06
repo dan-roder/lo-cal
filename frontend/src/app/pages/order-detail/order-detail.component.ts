442f2c5eb35e0bf6d766422d0210c3d51b968c87
Dan Roder <dsroder@gmail.com>
2018-07-05 20:40:43 -0400

    retrieving all data related to items ordered when looking at order history

--- frontend/src/app/pages/order-detail/order-detail.component.ts
+++ frontend/src/app/pages/order-detail/order-detail.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OrderService } from '@local/services/order.service';
 import { Order } from '@local/models/Order';
+import { BagService } from '@local/services/bag.service';
+import { WordpressService } from '@local/services/wp.service';
+import * as _ from 'lodash';
+import { LineItem } from '@local/models/LineItem';
 
 @Component({
   selector: 'lo-cal-order-detail',
@@ -10,20 +14,41 @@ import { Order } from '@local/models/Order';
 export class OrderDetailComponent implements OnInit {
   private orderId : number;
   public orderDetails : Order;
+  public menuMap : any;
+  public menuItemPosts : Array<any> = [];
 
-  constructor(private route: ActivatedRoute, private orderService: OrderService) {
+  constructor(private route: ActivatedRoute, private orderService: OrderService, private bagService: BagService, private wpService: WordpressService) {
     this.route.params.subscribe( params => this.orderId = params.orderid );
   }
 
   ngOnInit() {
     this.orderService.getFullOrderDetails(this.orderId).subscribe(orderDetails => {
-      console.log(orderDetails);
       this.orderDetails = orderDetails;
+
+      this.wpService.getMenuMapObject().subscribe(map => {
+        this.menuMap = map;
+        let wpService = this.wpService;
+        let menuItemArray = this.menuItemPosts;
+
+        _.forEach(orderDetails.LineItems, function(value){
+          let obj = _.find(map, {'menuid' : String(value.MenuItemId)});
+
+          wpService.getCustomPostTypeById('menu_item', obj.id).subscribe(post => {
+            menuItemArray.push(post);
+          })
+        })
+      })
     })
   }
 
   addOrderToBag(){
+    let bagService = this.bagService;
+    let menuItemPosts = this.menuItemPosts;
+    _.forEach(this.orderDetails.LineItems, function(lineItem, index){
+      lineItem.cartImage = (menuItemPosts[index].acf !== undefined && menuItemPosts[index].acf.cart_image !== undefined) ? menuItemPosts[index].acf.cart_image : 'http://via.placeholder.com/160x240';
 
+      bagService.orderItemAgain(lineItem);
+    })
   }
 
 }
