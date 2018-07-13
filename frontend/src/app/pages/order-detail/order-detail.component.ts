6c09c84dbddf2986226641a152c0779748bad032
Dan Roder <droder@repequity.com>
2018-07-13 15:12:46 -0400

    ensuring order was placed by currently logged in user

--- frontend/src/app/pages/order-detail/order-detail.component.ts
+++ frontend/src/app/pages/order-detail/order-detail.component.ts
@@ -5,38 +5,42 @@ import { Order } from '@local/models/Order';
 import { BagService } from '@local/services/bag.service';
 import { WordpressService } from '@local/services/wp.service';
 import * as _ from 'lodash';
+import { CustomerService } from '@local/services/customer.service';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
+import { Customer } from '@local/models/Customer';
 
 @Component({
   selector: 'lo-cal-order-detail',
   templateUrl: './order-detail.component.html'
 })
+
+@AutoUnsubscribe()
+
 export class OrderDetailComponent implements OnInit {
   private orderId : number;
   public orderDetails : Order;
   public menuMap : any;
   public menuItemPosts : Array<any> = [];
+  private currentUser : Customer;
+  private canSeeOrder : boolean = false;
 
-  constructor(private route: ActivatedRoute, private orderService: OrderService, private bagService: BagService, private wpService: WordpressService) {
+  constructor(private route: ActivatedRoute, private orderService: OrderService, private bagService: BagService, private wpService: WordpressService, private customerService: CustomerService) {
     this.route.params.subscribe( params => this.orderId = params.orderid );
   }
 
   ngOnInit() {
+    this.customerService.isLoggedIn().subscribe(user => {
+      this.currentUser = user;
+      this.getOrderDetails();
+    })
+  }
+
+  public getOrderDetails(){
     this.orderService.getFullOrderDetails(this.orderId).subscribe(orderDetails => {
       this.orderDetails = orderDetails;
-
-      this.wpService.getMenuMapObject().subscribe(map => {
-        this.menuMap = map;
-        let wpService = this.wpService;
-        let menuItemArray = this.menuItemPosts;
-
-        _.forEach(orderDetails.LineItems, function(value){
-          let obj = _.find(map, {'menuid' : String(value.MenuItemId)});
-
-          wpService.getCustomPostTypeById('menu_item', obj.id).subscribe(post => {
-            menuItemArray.push(post);
-          })
-        })
-      })
+      if(this.currentUser.CustomerId === this.orderDetails.Customer.CustomerId){
+        this.canSeeOrder = true;
+      }
     })
   }
 
@@ -49,5 +53,7 @@ export class OrderDetailComponent implements OnInit {
       bagService.orderItemAgain(lineItem);
     })
   }
+
+  ngOnDestroy(){}
 
 }
