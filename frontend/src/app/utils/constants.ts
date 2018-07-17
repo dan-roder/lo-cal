e61f2abd2335fa901e7cb13effa9ca797592c7ef
Dan Roder <dsroder@gmail.com>
2018-07-17 10:47:11 -0400

    updated api endpoints to domain and https

--- frontend/src/app/utils/constants.ts
+++ frontend/src/app/utils/constants.ts
@@ -4,12 +4,12 @@ import { WindowRef } from './windowref';
 @Injectable()
 export class Config {
   public wordpressApiUrl : string = '';
-  public railsMenuApi : string = 'http://138.197.123.70/menus/1';
-  public railsCustomerEndpoint : string = 'http://138.197.123.70/customers';
-  public authenticateEndpoint : string = 'http://138.197.123.70/authenticate';
-  public railsOrderEndpoint : string = 'http://138.197.123.70/orders';
-  public railsTimeEndpoint : string = 'http://138.197.123.70/times/1';
-  public allTimesEndpoint : string = 'http://138.197.123.70/times/1/1/0/1001';
+  public railsMenuApi : string = 'https://api.lo-calkitchen.com/menus/1';
+  public railsCustomerEndpoint : string = 'https://api.lo-calkitchen.com/customers';
+  public authenticateEndpoint : string = 'https://api.lo-calkitchen.com/authenticate';
+  public railsOrderEndpoint : string = 'https://api.lo-calkitchen.com/orders';
+  public railsTimeEndpoint : string = 'https://api.lo-calkitchen.com/times/1';
+  public allTimesEndpoint : string = 'https://api.lo-calkitchen.com/times/1/1/0/1001';
   public siteId : number = 1;
   public menuId : number = 1001;
   public paymentTypeMap = {
@@ -51,10 +51,10 @@ export class Config {
         this.wordpressApiUrl = "http://lo-cal.local/wp-json";
       break;
       case 'live-lo-cal.pantheonsite.io':
-        this.wordpressApiUrl = "http://www.lo-calkitchen.com/wp-json";
+        this.wordpressApiUrl = "https://www.lo-calkitchen.com/wp-json";
       break;
       default:
-        this.wordpressApiUrl = "http://" + winRef.nativeWindow.location.hostname + "/wp-json";
+        this.wordpressApiUrl = "https://" + winRef.nativeWindow.location.hostname + "/wp-json";
       break;
     }
 
