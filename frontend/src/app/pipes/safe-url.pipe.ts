994458f2ef23b43c411b184bf4918dbac1d66a19
Dan Roder <dsroder@gmail.com>
2018-07-18 10:25:31 -0400

    trimming white space

--- frontend/src/app/pipes/safe-url.pipe.ts
+++ frontend/src/app/pipes/safe-url.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SafeUrlPipe implements PipeTransform {
 
   transform(value: any): any {
-    let lowercase = value.toLowerCase();
+    let lowercase = value.toLowerCase().trim();
     let noChars = lowercase.replace(/[^a-z1-9 -]/g, '');
     let doubleHyphens = noChars.replace(/[-]{2,}/g, '-');
     let properUrl = doubleHyphens.replace(/\s+/g, '-');
