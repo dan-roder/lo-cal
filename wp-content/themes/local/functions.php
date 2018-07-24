0c84ed17544e3c4093b97287791877d819219809
Dan Roder <dsroder@gmail.com>
2018-07-24 19:07:16 -0400

    removed local origins from functions

--- wp-content/themes/local/functions.php
+++ wp-content/themes/local/functions.php
@@ -2,10 +2,3 @@
   require_once 'functions/setup.php';
   require_once 'functions/post_types.php';
   require_once 'functions/menu_sync_admin.php';
-
-add_filter( 'allowed_http_origins', 'add_allowed_origins' );
-function add_allowed_origins( $origins ) {
-    $origins[] = 'http://localhost:4200';
-    $origins[] = 'https://local.lndo.site';
-    return $origins;
-}
