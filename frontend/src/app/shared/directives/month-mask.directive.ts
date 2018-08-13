b3ac1568c663e4ea12c114d112e06f65131aff49
Dan Roder <dsroder@gmail.com>
2018-08-13 17:48:05 -0400

    slight changes to month mask directive

--- frontend/src/app/shared/directives/month-mask.directive.ts
+++ frontend/src/app/shared/directives/month-mask.directive.ts
@@ -8,15 +8,16 @@ import { NgControl } from '@angular/forms';
   }
 })
 export class MonthMaskDirective {
-  @Output() rawChange: EventEmitter<string> = new EventEmitter<string>();
+  // @Output() rawChange: EventEmitter<string> = new EventEmitter<string>();
 
   constructor(public model: NgControl) { }
 
   onInputChange(e, backspace){
-    let x = e.replace(/\D/g, '').match(/(\d{0,2})(\d{0,4})/);
+    let x = e.replace(/[^0-9]/g, '').match(/([0-9]{0,2})([0-9]{0,4})/);
     let rawValue = !x[2] ? x[1] : x[1] + '/' + x[2];
+    console.log(rawValue);
     // new value
     this.model.valueAccessor.writeValue( rawValue );
-    this.rawChange.emit( rawValue );
+    // this.rawChange.emit( rawValue );
   }
 }
