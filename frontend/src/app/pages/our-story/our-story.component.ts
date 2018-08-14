6187d1bae154f1f4a5710dfb9dfe50aae59f5c52
Dan Roder <dsroder@gmail.com>
2018-08-14 11:41:03 -0400

    added background image to section

--- frontend/src/app/pages/our-story/our-story.component.ts
+++ frontend/src/app/pages/our-story/our-story.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WordpressService } from '../../services/wp.service';
 import { AutoUnsubscribe } from "ngx-auto-unsubscribe";
 import { IPost } from '@local/models/post';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'lo-cal-our-story',
@@ -14,18 +15,25 @@ export class OurStoryComponent implements OnInit {
   public pageContent : IPost;
   public acf : any;
   public featuredImage : any;
+  public bgImage : string = '';
 
-  constructor(private wpService: WordpressService) { }
+  constructor(private wpService: WordpressService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.wpService.getCustomPostTypeById('landing_page', 80).subscribe(page => {
       this.pageContent = page;
       this.acf = page.acf;
+      this.bgImage = (page.acf.background_image !== undefined) ? page.acf.background_image.url : '';
 
       if(page.featured_media != 0){
         this.wpService.getMedia(page.featured_media).subscribe(media => this.featuredImage = media);
       }
     });
+  }
+
+  public getBgImage(){
+    let style = `background-image: url(${this.bgImage})`;
+    return this.sanitizer.bypassSecurityTrustStyle(style);
   }
 
   ngOnDestroy(){
