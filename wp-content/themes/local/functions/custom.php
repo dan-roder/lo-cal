80e4996febfd30736cfe29d1f0b560061d8a1817
Dan Roder <dsroder@gmail.com>
2018-08-14 14:54:08 -0400

    added custom endpoint for logging errors

--- wp-content/themes/local/functions/custom.php
+++ wp-content/themes/local/functions/custom.php
@@ -1,49 +1,23 @@
 <?php
 
-// function slug_get_post_meta_cb( $object, $field_name, $request ) {
-//   return get_post_meta( $object[ 'id' ], $field_name );
-// }
+add_action( 'rest_api_init', 'post_error_message' );
 
-// add_action( 'rest_api_init', function() {
-//   register_rest_field( 'menu_item', 'menuid',
-//     array(
-//       'get_callback'    => 'slug_get_post_meta_cb',
-//       'schema'          => null,
-//     )
-//   );
-
-//  });
-
-add_action( 'rest_api_init', 'get_menu_item_from_menu_id' );
-
-function get_menu_item_from_menu_id() {
-  register_rest_route( 'menu_item/v1', '/menuid/(?P<id>\d+)', array(
-    'methods' => 'GET',
-    'callback' => 'get_post_data_from_menu_id',
-    'args' => array(
-      'id' => array(
-        'validate_callback' => function($param, $request, $key) {
-          return is_numeric( $param );
-        }
-      ),
-    ),
+function post_error_message() {
+  register_rest_route( 'error_message/v2', 'log/', array(
+    'methods' => 'POST',
+    'callback' => 'log_error_message'
   ));
 }
 
-function get_post_data_from_menu_id( $object ) {
-    //get the id of the post object array
-    $posts = get_posts(array(
-      'numberposts'	=> -1,
-      'post_type'		=> 'menu_item',
-      'meta_key'		=> 'menuid',
-      'meta_value'	=> $object['id']
-    ));
+function log_error_message(WP_REST_Request $request){
+  $returnObj = new \stdClass();
 
-    if(isset($posts[0]->ID)){
-      $url = 'http://'.$_SERVER['HTTP_HOST'].'/wp-json/wp/v2/menu_item/' . $posts[0]->ID . '?_embed';
-      $body = wp_remote_retrieve_body(wp_remote_get($url));
-    }
+  if(function_exists('log_error')){
+    $bodyContent = $request->get_body();
+    $returnObj->response = 'error logged to wordpress error logs';
+    log_error($bodyContent);
+    log_error('test error');
+  }
+  return rest_ensure_response($returnObj);
 
-    //return the post
-    return new WP_REST_Response( json_decode($body), 200 );
 }
