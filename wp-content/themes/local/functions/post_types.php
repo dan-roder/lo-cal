d1b164de2455ac7036b96dbb2d4df60fd0a3a0e8
Dan Roder <dsroder@gmail.com>
2018-08-23 09:51:11 -0400

    Revert "re-made category hierarchical"
    This reverts commit 8cbb04e6095af52182beeae36bd0e7bc85343fee.

--- wp-content/themes/local/functions/post_types.php
+++ wp-content/themes/local/functions/post_types.php
@@ -181,7 +181,7 @@ function menu_categories() {
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor', 'thumbnail' ),
 		'taxonomies'            => array( 'category', 'post_tag' ),
-		'hierarchical'          => true,
+		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
