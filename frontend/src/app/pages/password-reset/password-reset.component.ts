1e414785ea9883e06d1ca2fc0831d6d4c7189212
Dan Roder <dsroder@gmail.com>
2018-08-31 11:41:52 -0400

    password reset component created

---
+++ frontend/src/app/pages/password-reset/password-reset.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lo-cal-password-reset',
+  templateUrl: './password-reset.component.html'
+})
+export class PasswordResetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
