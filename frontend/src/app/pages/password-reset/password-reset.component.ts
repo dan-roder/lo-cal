284746e8c19dd15164a78f9f1bb80507e2612d15
Dan Roder <dsroder@gmail.com>
2018-08-31 14:56:33 -0400

    started password reset form

--- frontend/src/app/pages/password-reset/password-reset.component.ts
+++ frontend/src/app/pages/password-reset/password-reset.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lo-cal-password-reset',
   templateUrl: './password-reset.component.html'
 })
 export class PasswordResetComponent implements OnInit {
+  public passwordResetForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+
+  }
+
+
 
   ngOnInit() {
   }
