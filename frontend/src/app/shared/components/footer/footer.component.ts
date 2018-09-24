9c8ca14995523619471eff26f5d008146fd8693c
Dan Roder <dsroder@gmail.com>
2018-09-24 15:15:19 -0400

    calling mailchimp service on submit

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { WordpressService } from '../../../services/wp.service';
+import { WordpressService } from '@local/services/wp.service';
+import { MailchimpService } from '@local/services/mailchimp.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'lo-cal-footer',
@@ -8,13 +11,31 @@ import { WordpressService } from '../../../services/wp.service';
 export class FooterComponent implements OnInit {
   public footerMenu;
   public footerLinks: any;
+  public newsletterForm: FormGroup;
 
-  constructor(private wpService : WordpressService) {  }
+  constructor(private wpService : WordpressService, private fb: FormBuilder, private mailchimpService: MailchimpService) {
+    this.newsletterForm = fb.group({
+      'email' : ['', [Validators.required, Validators.email]]
+    })
+  }
 
   ngOnInit() {
     this.wpService.getMenu(3).subscribe(m => this.footerMenu = m.items);
     this.wpService.getMenu(4).subscribe(m => {
       this.footerLinks = m.items
     });
+  }
+
+  public signUp(formData){
+    if(formData.valid){
+      let email = formData.get('email').value;
+
+      this.mailchimpService.signUpForNewsletter(email).subscribe(result => {
+        console.log(result);
+      }, error => {
+        console.log(error);
+      });
+    }
+    return false;
   }
 }
