138db01082190ca0a15d18a09b6c2beb43d40009
Dan Roder <dsroder@gmail.com>
2018-09-24 19:07:00 -0400

    added endpoint for submitting to mailchimp

--- wp-content/themes/local/functions/custom.php
+++ wp-content/themes/local/functions/custom.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * REST API endpoint for posting error messages to Wordpress plugin
+ */
 add_action( 'rest_api_init', 'post_error_message' );
 
 function post_error_message() {
@@ -21,6 +24,9 @@ function log_error_message(WP_REST_Request $request){
 
 }
 
+/**
+ * REST API Endpoint for adding a contact form submission to a custom post type
+ */
 add_action( 'rest_api_init', 'submit_contact_form' );
 
 function submit_contact_form(){
@@ -72,4 +78,67 @@ function handle_contact_submission(WP_REST_Request $request){
   else{
     return 'Error';
   }
+}
+
+/**
+ * REST API Endpoint for submitting a user to Mailchimp
+ */
+
+add_action( 'rest_api_init', 'mailchimp_signup' );
+
+function mailchimp_signup() {
+  register_rest_route( 'mailchimp/v2', 'signup/', array(
+    'methods' => 'POST',
+    'callback' => 'signup_for_newsletter'
+  ));
+}
+
+function signup_for_newsletter(WP_REST_Request $request){
+  $mailChimpObj = new \stdClass();
+  $body = $request->get_body();
+
+  if($body){
+    define('MAILCHIMP_ENDPOINT', 'https://us14.api.mailchimp.com/3.0/lists/1ab7655e84/members');
+    define('API_KEY', '44a3b36ccc5b4109a29346f9c892b443-us14');
+
+    $json = json_decode($body, true);
+
+    $mailChimpObj->email_address = $json['email_address'];
+    $mailChimpObj->status = $json['status'];
+
+    $payload = json_encode($mailChimpObj);
+
+    $ch = curl_init();
+
+    $headers = [];
+    //set the url, number of POST vars, POST data
+    $options = array(
+      CURLOPT_HEADER => true,
+      CURLOPT_HTTPHEADER => array('Accept: application/json'),
+      CURLOPT_USERPWD => "local:" . API_KEY,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_URL => MAILCHIMP_ENDPOINT
+    );
+
+    $options[CURLOPT_POST] = true;
+    $options[CURLOPT_POSTFIELDS] = $payload;
+
+    //execute post
+
+    curl_setopt_array($ch, $options);
+    $result = curl_exec($ch);
+
+    if (curl_error($ch)) {
+      $error_msg = curl_error($ch);
+      return rest_ensure_response($error_msg);
+    }
+
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $headers = substr($result, 0, $header_size);
+    $respBody = substr($result, $header_size);
+  }
+
+  //close connection
+  return rest_ensure_response($respBody);
+
 }
