bd03df55d172f5b55d72ce9ba31e2ffa9cbdf53b
Dan Roder <dsroder@gmail.com>
2018-09-24 19:07:59 -0400

    handling response from mailchimp signup

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -12,6 +12,8 @@ export class FooterComponent implements OnInit {
   public footerMenu;
   public footerLinks: any;
   public newsletterForm: FormGroup;
+  public signUpResponse: string = '';
+  public signUpSuccess: boolean = false;
 
   constructor(private wpService : WordpressService, private fb: FormBuilder, private mailchimpService: MailchimpService) {
     this.newsletterForm = fb.group({
@@ -31,9 +33,25 @@ export class FooterComponent implements OnInit {
       let email = formData.get('email').value;
 
       this.mailchimpService.signUpForNewsletter(email).subscribe(result => {
-        console.log(result);
+        let resp = JSON.parse(result);
+
+        switch(resp.title){
+          case 'Member Exists':
+            this.signUpResponse = `Looks like you're already signed up!`;
+            this.signUpSuccess = true;
+          break;
+          default:
+            if(resp.id != undefined || resp.id != null){
+              this.signUpSuccess = true;
+              this.signUpResponse = `Thank you for signing up!`;
+            }
+            else{
+              this.signUpResponse = `There was an unexpected error. Try again.`;
+            }
+          break;
+        }
       }, error => {
-        console.log(error);
+        this.wpService.logError('Newsletter Error: ' + JSON.stringify(error)).subscribe(() => {});
       });
     }
     return false;
