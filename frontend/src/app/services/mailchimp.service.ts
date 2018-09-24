d2b2742d0ff15cb665e86e434c685fa2848529bb
Dan Roder <dsroder@gmail.com>
2018-09-24 19:07:48 -0400

    submitting newsletter form to wp endpoint

--- frontend/src/app/services/mailchimp.service.ts
+++ frontend/src/app/services/mailchimp.service.ts
@@ -1,37 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Config } from '@local/utils/constants';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { MailchimpSignup } from '@local/models/Customer';
 
 @Injectable()
 export class MailchimpService {
-  private httpOptions: any;
-  protected apiKey: string = '44a3b36ccc5b4109a29346f9c892b443-us14';
-
   constructor(private constants: Config, private http: HttpClient) {
 
   }
 
   public signUpForNewsletter(email: string): Observable<any>{
-    let httpHeaders = new HttpHeaders();
-    httpHeaders.append('Content-Type', 'application/json');
-    httpHeaders.append('Access-Control-Allow-Origin', '*');
-    httpHeaders.append('Authorization', 'Basic ' + btoa('local:' + this.apiKey));
-
-    this.httpOptions = {
-      headers: httpHeaders
-    }
-
     let payload: MailchimpSignup = {
       email_address : email,
       status : "subscribed"
     }
 
-    console.log(httpHeaders);
-    console.log(payload, this.httpOptions);
-
-    return this.http.post(this.constants.mailchimpEndpoint, payload, {headers: httpHeaders}).map(result => {
+    let json = JSON.stringify(payload);
+    return this.http.post(this.constants.wordpressApiUrl + `/mailchimp/v2/signup`, json).map(result => {
       return result;
     });
   }
