f50ed5c4545033670cb9c497da8a27fd537af5d0
Dan Roder <dsroder@gmail.com>
2018-10-02 09:57:40 -0400

    added text beyond allergens to pipe

--- frontend/src/app/pipes/allergens.pipe.ts
+++ frontend/src/app/pipes/allergens.pipe.ts
@@ -14,16 +14,22 @@ export class AllergensPipe implements PipeTransform {
     //  Item 1 is the full description
     //  Item 2 is the allergens
     if(value.indexOf('[') !== -1){
-      let strArray = value.split('[');
-      obj['description'] = strArray[0];
+      let matches = value.match(/(.*)(\[.*\])(.*)/);
+      obj['description'] = matches[1].trim();
 
       // If the item has a ] in it slice it off so we're left with just allergen codes
-      if(strArray[1] !== undefined){
-        strArray[1] = strArray[1].slice(0, -1);
+      if(matches[2] !== undefined){
+        matches[2] = matches[2].slice(0, -1);
+        matches[2] = matches[2].substr(1);
         // Split allergens into subArray based on underscores
-        strArray[1] = strArray[1].split('_');
+        matches[2] = matches[2].split('_');
 
-        obj['allergens'] = strArray[1];
+        obj['allergens'] = matches[2];
+      }
+
+      // If a 3rd matches group exists there was text beyond the allergens
+      if(matches[3] !== undefined){
+        obj['additionalInfo'] = matches[3].trim();
       }
     }
     else{
