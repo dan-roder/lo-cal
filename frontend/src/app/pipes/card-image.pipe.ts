ee7406a5c34e35c33695861c34bcb549ff0052d0
Dan Roder <dsroder@gmail.com>
2018-11-20 13:24:11 -0500

    faster return of card image

--- frontend/src/app/pipes/card-image.pipe.ts
+++ frontend/src/app/pipes/card-image.pipe.ts
@@ -1,22 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { WordpressService } from '@local/services/wp.service';
 import * as _ from 'lodash';
-import { Observable } from 'rxjs';
 
 @Pipe({
   name: 'cardImage'
 })
 export class CardImagePipe implements PipeTransform {
 
-  constructor(private wpService: WordpressService) { }
+  constructor() { }
 
   transform(alohaMenuId: any, menuItemMap?: any) {
     if(alohaMenuId) {
       let postObj = _.find(menuItemMap, {'menuid' : String(alohaMenuId)});
 
       if(postObj !== undefined){
+        let returnObj = {};
         if(postObj.submenu_image !== undefined){
-          return postObj.submenu_image.url;
+          returnObj['submenu_image'] = postObj.submenu_image.url;
+          returnObj['submenu_alt'] = postObj.submenu_image.alt;
+          return returnObj;
         }
         else{
           return '';
